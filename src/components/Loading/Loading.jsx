076baf8f231e2d7a649272f2441d9f7939b8ec5a
zahyaah <itszayd2003@gmail.com>
2024-08-13 00:04:38 +0530

    mod Loading animation

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,20 +2,21 @@ import { motion } from 'framer-motion';
 
 function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen relative">
       <motion.div
-        className="inline-block w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full"
-        animate={{
-          rotate: 360,
-        }}
+        className="h-12 w-12 bg-[#8785A2] rounded-full"
+        initial={{ y: 0 }}
+        animate={{ y: [0, window.innerHeight / 2 - 24, 0] }}
         transition={{
           repeat: Infinity,
-          duration: 0.8,
-          ease: 'linear',
+          duration: 1,
+          ease: "easeInOut",
+          repeatType: "loop",
+          bounce: 0.5
         }}
       />
     </div>
   );
-};
+}
 
 export default Loading;
