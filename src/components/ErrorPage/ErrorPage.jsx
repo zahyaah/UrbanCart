1d234f2d97c58724d23108e92b1c0ab8c6914d8f
zahyaah <itszayd2003@gmail.com>
2024-08-13 15:27:46 +0530

    add ErrorPage

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+function ErrorPage() {
+    return (
+        <div className="h-screen w-full overflow-x-hidden overflow-y-hidden bg-red-900 flex items-center">
+            <p className="text-5xl text-black">ERROR INVALID PAGE</p>
+        </div>
+    )
+}
+
+export default ErrorPage; 
